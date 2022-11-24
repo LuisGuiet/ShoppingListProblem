adefae13876a851a263a98b566aebc4eb9ca6fbc
Luis Guiet <GUI202201133@longroad.ac.uk>
2022-11-24 10:16:48 +0000

    creates files, can add item to list and closes the file.

--- ShoppingListProblem/Program.cs
+++ ShoppingListProblem/Program.cs
@@ -35,15 +35,14 @@ namespace ShoppingListProblem
             {
                 // Creates the text file.
                 TextWriter TextFile = new StreamWriter(_FileName);
-                NewShoppingList(TextFile);
+                NewShoppingList(TextFile, _FileName);
             }
         }
 
-        static void NewShoppingList(TextWriter SelectedFile)
+        static void NewShoppingList(TextWriter SelectedFile, string FileName)
         {
             string _UserInput = "";
-            bool _CanContinue = false;
-            string[ , ] _NewItem = new string[ , ] { };
+            bool _CanContinue = true;
 
             // Ask if they want to add a new item, delete an item or output the file.
 
@@ -51,7 +50,8 @@ namespace ShoppingListProblem
             {
                 Console.WriteLine("1 > Add item to list. \n" +
                               "2 > Remove item from list. \n" +
-                              "3 > Output list to console.\n");
+                              "3 > Output list to console.\n" +
+                              "4 > Closes the file.\n");
 
                 _UserInput = Console.ReadLine();
 
@@ -59,7 +59,33 @@ namespace ShoppingListProblem
                 {
                     case "1":
                         // Adds an item to the list and the quantity.
+                        string _NewItemName = "";
+                        string _NewItemQuantity = "";
 
+                        // Request the name and quantity.
+                        Console.WriteLine("Please type the item name...\n");
+                        _NewItemName = Console.ReadLine();
+
+                        Console.WriteLine("Please type the item quantity...\n");
+                        _NewItemQuantity = Console.ReadLine();
+
+                        // Check that the string is not an int.
+                        if (!Int32.TryParse(_NewItemName, out int push))
+                        {
+                            // Check that the int is not char.
+                            if (Int32.TryParse(_NewItemQuantity, out int push2))
+                            {
+                                SelectedFile.WriteLine(_NewItemName + _NewItemQuantity);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Item quantity cannot consist of characters.\n");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Item name cannot consist of only integers.\n");
+                        }
                         break;
 
                     case "2":
@@ -72,8 +98,14 @@ namespace ShoppingListProblem
 
                         break;
 
-                    default:
+                    case "4":
+                        // Closes the file.
+                        SelectedFile.Close();
                         _CanContinue = false;
+                        break;
+
+                    default:
+                        _CanContinue = true;
                         break;
                 }
             }
